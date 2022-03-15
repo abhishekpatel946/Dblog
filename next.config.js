e5b92fdd61c5e2e7371d89e03ce38b3e7466ba70
Abhishek Patel <abhishekpatel946@gmail.com>
2022-03-15 22:07:22 +0530

    Initial setup the dblog with contract & tested locally

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+require('@nomiclabs/hardhat-waffle');
 
-module.exports = nextConfig
+module.exports = {
+  solidity: '0.8.4',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    // mumbai: {
+    //   url: "https://rpc-mumbai.matic.today",
+    //   accounts: [process.env.pk]
+    // },
+    // polygon: {
+    //   url: "https://polygon-rpc.com/",
+    //   accounts: [process.env.pk]
+    // }
+  },
+};
